e030665484d0c53417a19647f57fc34f7548857b
Sadiq Ramanu <abdulrahmansadiq13@gmail.com>
2026-04-05 14:39:37 -0400

    added ownerOnly modirier, mappings, and initial card buying logic

--- contracts/pokemonsc.sol
+++ contracts/pokemonsc.sol
@@ -15,7 +15,7 @@ contract PokemonFTCG {
      * Auctions
     
     */
-
+    
     struct Card {
         string name;
         uint256 attack;
@@ -33,6 +33,26 @@ contract PokemonFTCG {
     }
 
     mapping(uint256 =>  Card) public cards;
+    mapping(string => string) public cardToImage;
+    mapping(uint256 => address) internal _ownerOf;
+    mapping(address => to uint256[]) private tokenOwnerstoIds;
+    mapping(address => uint256) internal _balanceOf;
+    mapping(uint256 => address) internal _approvals;
+    mapping(address => mapping(address => bool)) public isApprovedForAll;
+
+    constructor(){}
+
+    // Modifiers
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "Not the owner");
+        _;
+    }
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 tokenId);
+    event Approval(address indexed owner, address indexed buyer, uint256 tokenId);
+    event ApprovedForAll(address indexed owner, address indexed operator, bool approved);
+
 
     // Probably only admin or owner should mint cards
     function _mintCard(address to, uint256 cardId) internal{
@@ -43,10 +63,27 @@ contract PokemonFTCG {
         emit Transfer(address(0), to, id);
     }
 
-    function  listCard()
+    function  listCard(){}
 
-    function buyCard()
+    function buyCard(uint256 cardId) public {
+        require(_ownerOf[cardId] != address(0), "Card not minted");
+        require(_ownerOf[cardId] != msg.sender, "You already own this card");
+        require(listings[cardId].price > 0, "Card not listed for sale");
+        require(listings[cardId].price <= msg.value, "Insufficient funds");
+        address owner = _ownerOf[cardId];
+        uint256 price = listings[cardId].price;
+        delete listings[cardId];
+        _ownerOf[cardId] = msg.sender;
+        _balanceOf[owner] --;
+        _balanceOf[msg.sender] ++;
+        tokenOwnerstoIds[msg.sender].push(cardId);
+        emit Transfer(owner, msg.sender, cardId);
+        payable(owner).transfer(price);
 
+
+    }
+
+    
 
 
 
