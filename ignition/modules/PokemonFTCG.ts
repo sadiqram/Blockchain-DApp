99c36bcb8123b62327aade166ca0c4adefb6157d
sadiqram <abdulrahmansadiq13@gmail.com>
2026-04-30 10:07:24 -0400

    add script for testing script deployment

---
+++ ignition/modules/PokemonFTCG.ts
@@ -0,0 +1,14 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const PokemonModule = buildModule("PokemonModule", (m) => {
+  // 1. Deploy Yoda token first
+  const yoda = m.contract("ERC20Token", [100, 18]);
+  // (initialReceiveAmount = 100, decimals = 18)
+
+  // 2. Deploy Pokemon contract with Yoda address
+  const pokemon = m.contract("PokemonFTCG", [yoda]);
+
+  return { yoda, pokemon };
+});
+
+export default PokemonModule;
